3255cbf79d8c6282f785f70e6dfd98a684284e44
Alejandro Yujra <alejandroyujra8@gmail.com>
2025-07-21 23:38:24 -0400

    Persona: clase creada con atributos y constructor

--- src/main/java/com/mycompany/proyecto/Persona.java
+++ src/main/java/com/mycompany/proyecto/Persona.java
@@ -1,5 +1,13 @@
 package com.mycompany.proyecto;
 
 public class Persona {
-    
+    protected String nombre;
+    protected int ci;
+    protected int edad;
+
+    public Persona(String nombre, int ci, int edad) {
+        this.nombre = nombre;
+        this.ci = ci;
+        this.edad = edad;
+    }
 }
