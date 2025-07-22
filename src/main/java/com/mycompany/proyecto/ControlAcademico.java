758ec0ee73492d51b5de1bcecfc048c5b08643b0
Bryan Daniel Sandoval Quenta <dan.san.q.gt@gmail.com>
2025-07-21 22:34:23 -0400

    ControlAcademico: creacion de clase con vectores de estudiantes y docentes, y metodo mostrar

--- src/main/java/com/mycompany/proyecto/ControlAcademico.java
+++ src/main/java/com/mycompany/proyecto/ControlAcademico.java
@@ -1,5 +1,26 @@
 package com.mycompany.proyecto;
 
 public class ControlAcademico {
+    private Estudiante vecEst[];
+    private int nroEst;
+    private Docente vecDoc[];
+    private int nroDoc;
+
+    public ControlAcademico() {
+        this.nroEst = 0;
+        this.nroDoc = 0;
+        vecEst = new Estudiante[100];
+        vecDoc = new Docente[50];
+    }   
     
+    public void mostrar(){
+        System.out.println("nro de Estudiantes: " + nroEst + ", nro de Docentes: " + nroDoc);
+        for(int i = 0; i < nroEst; i++){
+            vecEst[i].mostrar();
+        }
+        
+        for(int j = 0; j < nroDoc; j++){
+            vecDoc[j].mostrar();
+        }
+    }
 }
