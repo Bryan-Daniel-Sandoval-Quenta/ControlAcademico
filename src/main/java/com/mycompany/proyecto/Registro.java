b79948ae54d75a6bbdeb801851964e1d3377bb38
Alejandro Yujra <alejandroyujra8@gmail.com>
2025-07-22 00:21:18 -0400

    Registro: creada clase genérica con atributos, constructor, getters y setters.

--- src/main/java/com/mycompany/proyecto/Registro.java
+++ src/main/java/com/mycompany/proyecto/Registro.java
@@ -1,5 +1,38 @@
 package com.mycompany.proyecto;
 
 public class Registro<T> {
-    
+    private T[] vecReg;
+    private int nroReg;
+    private String fechaModificacion;
+
+    public Registro(T[] vecReg, int nroReg, String fechaModificacion) {
+        this.vecReg = vecReg;
+        this.nroReg = nroReg;
+        this.fechaModificacion = fechaModificacion;
+    }
+
+
+    public T[] getVecReg() {
+        return vecReg;
+    }
+
+    public void setVecReg(T[] vecReg) {
+        this.vecReg = vecReg;
+    }
+
+    public int getNroReg() {
+        return nroReg;
+    }
+
+    public void setNroReg(int nroReg) {
+        this.nroReg = nroReg;
+    }
+
+    public String getFechaModificacion() {
+        return fechaModificacion;
+    }
+
+    public void setFechaModificacion(String fechaModificacion) {
+        this.fechaModificacion = fechaModificacion;
+    }
 }
