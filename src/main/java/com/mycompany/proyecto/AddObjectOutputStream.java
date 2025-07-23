991b2dd6d228e86f3f609993193ba6dad2f3c2f6
Bryan Daniel Sandoval Quenta <dan.san.q.gt@gmail.com>
2025-07-22 23:52:10 -0400

    AddObjectOutputStream: creacion de la clase para evitar duplicados de cabecera al adicionar objetos

---
+++ src/main/java/com/mycompany/proyecto/AddObjectOutputStream.java
@@ -0,0 +1,14 @@
+package com.mycompany.proyecto;
+
+import java.io.*;
+
+public class AddObjectOutputStream extends ObjectOutputStream{
+    public AddObjectOutputStream(OutputStream out) throws IOException{
+        super(out);
+    }
+
+    @Override
+    protected void writeStreamHeader() throws IOException{
+        this.reset();
+    }
+}
