d57a8e238ad8cdce5d2063565beefe71ccd7cc7d
Alejandro Yujra <alejandroyujra8@gmail.com>
2025-07-22 22:05:27 -0600

    CLASE COORDINADOR2

--- src/main/java/com/mycompany/proyecto/Coordinador.java
+++ src/main/java/com/mycompany/proyecto/Coordinador.java
@@ -4,21 +4,25 @@ public class Coordinador extends Persona {
     private String semestreCoordinado;
     private String areaResponsable;
     
-    public Coordinador(String nombre,int ci,int edad,String tipodocente,double sueldo,
-            int nroMateria,String semestreCoordinado,String areaResponsable)
-    {   super(nombre,ci,edad,tipodocente,sueldo,nroMateria);
-        this.semestreCoordinado=semestreCoordinado;
-        this.areaResponsable=areaResponsable;
-        
+    public Coordinador(String semestreCoordinado, String areaResposable, String nombre, int ci, int edad, String tipoDocente, double sueldo, int nroMateria) {
+        super(nombre, ci, edad, tipoDocente, sueldo, nroMateria);
+        this.semestreCoordinado = semestreCoordinado;
+        this.areaResponsable = areaResposable;
     }
     public void leer()
     {   Scanner sc=new Scanner(System.in);
+    
+        System.out.println("Semestre Coordinado");
+        semestreCoordinado=sc.nextLine();
         
-        sout
+        System.out.println("Area Responsable: ");
+        areaResponsable=sc.nextLine();
+        
     }
     
     
     public void mostrar()
-    {
+    { System.out.println("Smestre Coordinador: "+semestreCoordinado);
+        System.out.println("Area Responsable: "+areaResponsable);
     }
 }
