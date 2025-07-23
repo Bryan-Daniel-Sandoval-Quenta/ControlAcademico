65600eaf31eed43c527d12e17ca1b35d4beaa890
fernanda7v <delma1234fernanda@gmail.com>
2025-07-22 22:01:15 -0600

    CLASE COORDINADOR

--- src/main/java/com/mycompany/proyecto/Coordinador.java
+++ src/main/java/com/mycompany/proyecto/Coordinador.java
@@ -1,5 +1,24 @@
 package com.mycompany.proyecto;
-
-public class Coordinador {
+import java.util.Scanner;
+public class Coordinador extends Persona {
+    private String semestreCoordinado;
+    private String areaResponsable;
     
+    public Coordinador(String nombre,int ci,int edad,String tipodocente,double sueldo,
+            int nroMateria,String semestreCoordinado,String areaResponsable)
+    {   super(nombre,ci,edad,tipodocente,sueldo,nroMateria);
+        this.semestreCoordinado=semestreCoordinado;
+        this.areaResponsable=areaResponsable;
+        
+    }
+    public void leer()
+    {   Scanner sc=new Scanner(System.in);
+        
+        sout
+    }
+    
+    
+    public void mostrar()
+    {
+    }
 }
